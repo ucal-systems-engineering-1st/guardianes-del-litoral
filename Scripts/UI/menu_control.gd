04ad97f22b0e2f1d582ef394f0657b3ef241738d
Ivan Hermilio Mera Ibarguen <129082143+1v4nz2023@users.noreply.github.com>
2026-06-08 19:03:40 -0500

    feat: tortuga

--- Scripts/UI/menu_control.gd
+++ Scripts/UI/menu_control.gd
@@ -4,16 +4,16 @@ extends Control
 @onready var btn_salir = $VBoxContainer/BtnSalir
 
 func _ready():
-    # Conectar botones
-    btn_jugar.pressed.connect(_on_btn_jugar_pressed)
-    btn_salir.pressed.connect(_on_btn_salir_pressed)
+	# Conectar botones
+	btn_jugar.pressed.connect(_on_btn_jugar_pressed)
+	btn_salir.pressed.connect(_on_btn_salir_pressed)
 
 func _on_btn_jugar_pressed():
-    var tween = create_tween()
-    tween.tween_property(self, "modulate:a", 0, 0.5)
-    await tween.finished
-    get_tree().change_scene_to_file("res://Scenes/Rooms/Intro.tscn")
+	var tween = create_tween()
+	tween.tween_property(self, "modulate:a", 0, 0.5)
+	await tween.finished
+	get_tree().change_scene_to_file("res://Scenes/Rooms/Intro.tscn")
 
 func _on_btn_salir_pressed():
-    # Cerrar el juego
-    get_tree().quit()
+	# Cerrar el juego
+	get_tree().quit()
